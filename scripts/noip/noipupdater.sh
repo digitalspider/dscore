36745599864631fb7c65aa51b55e7f34962c16a9
Development Team <dev@digitalspider.com.au>
2014-08-25 10:12:32 +1000

    noip updater

---
+++ scripts/noip/noipupdater.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# No-IP uses emails as passwords, so make sure that you encode the @ as %40
+SCRIPTDIR=/opt/digitalspider/dscore/scripts
+NOIPDIR=$SCRIPTDIR/noip
+USERNAME=dvittor
+AUTHORIZATION=`cat /.secret`
+HOST=vittor.ddns.net
+LOGFILE=$NOIPDIR/noip.log
+STOREDIPFILE=$NOIPDIR/noip.currentip.txt
+USERAGENT="Simple Bash No-IP Updater/0.4 dvittor@gmail.com"
+
+if [ ! -e $STOREDIPFILE ]; then 
+	touch $STOREDIPFILE
+fi
+
+NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
+STOREDIP=$(cat $STOREDIPFILE)
+
+if [ "$NEWIP" != "$STOREDIP" ]; then
+	RESULT=$(curl -o "$LOGFILE" -s -A "$USERAGENT" -H "Host: dynupdate.no-ip.com" -H "Authorization: Basic $AUTHORIZATION" "http://dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
+
+	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
+	echo $NEWIP > $STOREDIPFILE
+else
+	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
+fi
+
+echo $LOGLINE >> $LOGFILE
+
+exit 0
